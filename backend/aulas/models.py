4d0e0085d72f002b44d1ce3b659ab66a4074c124
Cristian Ruppel <licenciadocristianruppel@gmail.com>
2026-06-02 15:55:51 -0300

    Add main aulas models

--- backend/aulas/models.py
+++ backend/aulas/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=128)
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=128)
+    apellido = models.CharField(max_length=128)
+    mostrar = models.CharField(max_length=256)
+
+
+class Aula(models.Model):
+    descripcion = models.CharField(max_length=128)
+    ubicacion = models.CharField(max_length=128)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
+
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=128)
+    cant_alumnos = models.IntegerField(default=5)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
